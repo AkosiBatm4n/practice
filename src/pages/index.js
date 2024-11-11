852eed06793a4c9bcf485ded029f39b3a77f9cb6
Ian Vincent L. Reyes <99298221+IanReyes2@users.noreply.github.com>
2024-11-12 00:33:18 +0800

    3rd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
 
 import Content from "./components/Content";
 import HeroSection from "./components/HeroSection";
+import Team from './components/Team';
+
+const queryClient = new QueryClient();
 
 export default function Home() {
   return (
-    <div>
-      <HeroSection/>
-      <Content/>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <HeroSection/>
+        <Team/>
+        <Content/>
+      </div>
+    </QueryClientProvider>
   );
 }
